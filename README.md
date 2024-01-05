3cdda7e843e9ec8db4e008add251aad0419736b6
nycotin <ntincani@hotmail.it>
2024-01-05 17:28:10 +0100

    Uploading final version

--- README.md
+++ README.md
@@ -1,14 +1,31 @@
-# climbing-website-CCS
+# climbing-website-HTML-CSS-JS
 
-# This is an HTML website about climbing styled by Nicol and Anni.
-# It consists of four pages:
-# - index.html
-# - bouldering.html
-# - climbing.html
-# - confirmation.html
-# It also includes some tables, links and forms to register to the courses.
-# 
-# Style was added as separate CSS file, which include:
-# navbar, external font, generic style, flex boxes, pseudo classes and media queries for responsive design.
-#
-# JS part to be added
+This is a website about climbing created in different stages and evolved after a series of code challenges.
+
+### HTML
+This project consists of four HTML pages, made in collaboration with Jannik:
+- index.html
+- bouldering.html
+- climbing.html
+- confirmation.html
+It also includes some tables, links and forms to register to the courses.
+
+### CSS
+The pages where styled with a separate CSS file, created in collaboration with Najma, which includes:
+ - navbar, external font, generic style, flex boxes, pseudo classes and media queries for responsive design.
+
+### JavaScript
+A JavaScript file was added for improved client-side form validation, which includes the display of error messages implemented by:
+- disabling the submit button;
+- turning background color of input to red;
+- displying of clear error message.
+
+The error messages are triggered on submission if one of the following conditions applies:
+- input/select value left empty;
+- firstname or lastname not capitalized;
+- email not containing the '@' symbol and not ending with '.com';
+- phone number contains characters opther than digits;
+
+If the validation is passed, an alert message with the submitted information is shown and the confirmation HTML is opened.
+
+In addition, the red background and error message are removed when the "red" input is on focus for "correction".
--- app.js
+++ app.js
@@ -1,33 +1,67 @@
+// Global cons and vars
 // Get submit button element
 const button = document.querySelector('.submit-btn');
 
 // Add button to event listener to trigger script on click
 button.addEventListener('click', displayInfo);
 
+// Get user input
+const labels = document.querySelectorAll('label');
+const inputs = document.querySelectorAll('input, select');
+let redInput;
+
+// Create p for showing error if necessary
+let form = document.querySelector('form.enrollment-form');
+let p = document.createElement('p');
+p.setAttribute('class', 'error');
 
 // Define function to be triggred on click
 function displayInfo () {
     // Remove previousoly added error 'p'
     let p = document.querySelector('p.error');
+    
     if (p != undefined || p != null) {
         p.remove();
     }
-    
-    // Get user input
-    const labels = document.querySelectorAll('label');
-    const inputs = document.querySelectorAll('input, select');
+
+    // Remove previously added disabling button
+    if (button.getAttribute('type') == 'button') {
+        button.setAttribute('type', 'submit');
+    }
 
     for (let i = 0; i < inputs.length; i++) {
+        // Remove whitespaces
+        inputs[i].value = inputs[i].value.trim();
+
+        // Check and alert if input is empty
         if (inputs[i].value == '') {
-            button.setAttribute('type', 'button');
-            let form = document.querySelector('form.enrollment-form');
-            let p = document.createElement('p');
-            p.setAttribute('class', 'error');
-            p.innerHTML = `${labels[i].innerHTML.replace(': ', '')} missing!`;
-            form.appendChild(p);
-            p.style.color = 'rgb(112, 7, 7)';
-            p.style.fontWeight = '900';
+            displayError(inputs[i], labels[i].innerHTML.replace(': ', ''));
             return;
+        } else if (inputs[i].id == 'first-name' || inputs[i].id == 'last-name') {
+            // Check that first/last-name is capitalized
+            const string = inputs[i].value;
+
+            if (string.charCodeAt(0) < 65 || string.charCodeAt(0) > 90) {
+                displayError(inputs[i], labels[i].innerHTML.replace(': ', ''));
+                return;
+            }
+        } else if (inputs[i].id == 'email') {
+            const string = inputs[i].value;
+            const symbol = '@';
+            const end = '.com';
+
+            if (string.includes(symbol) == false || string.includes(end) == false) {
+                displayError(inputs[i], labels[i].innerHTML.replace(': ', ''));
+                return;
+            }
+        } else if (inputs[i].id == 'phone') {
+            let phone = inputs[i].value;
+            for (let x = 0; x < phone.length; x++) {
+                if (phone.charCodeAt(x) < 48 || phone.charCodeAt(x) > 57) {
+                    displayError(inputs[i], labels[i].innerHTML.replace(': ', ''));
+                    return;
+                }
+            }
         }
     }
 
@@ -37,4 +71,33 @@ function displayInfo () {
     ${labels[2].innerHTML} ${inputs[2].value}
     ${labels[3].innerHTML} ${inputs[3].value}
     ${labels[4].innerHTML} ${inputs[4].value}`);
+}
+
+
+function displayError (input, label) {
+    // Disable submit
+    button.setAttribute('type', 'button');
+
+    // Add and custom error message
+    input.style.backgroundColor = 'rgb(112, 7, 7)';
+    if (input.value == '') {
+        p.innerHTML = `${label} missing!`;
+    } else if (input.id == 'first-name' || input.id == 'last-name') {
+        p.innerHTML = `${label} must be capitalized!`;
+    } else if (input.id == 'email') {
+        p.innerHTML = `Invalid ${label}!`;
+    } else if (input.id == 'phone') {
+        p.innerHTML = `${label} must contain only digits!`;
+    }
+    form.appendChild(p);
+    p.style.color = 'rgb(112, 7, 7)';
+    p.style.fontWeight = '900';
+
+    // Add red input to eventlistener
+    redInput = document.querySelector(`[style^="background-color"]`);
+    redInput.addEventListener('focusin', function () {
+        input.style.backgroundColor = '';
+        p.remove();
+    })
+
 }
